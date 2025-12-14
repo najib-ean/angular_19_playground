0d0c9662aec7ac49422bf5feb74ec43fcefc941f
fuadnajibullah <najibullah89@gmail.com>
2025-12-14 20:42:54 +0700

    update materi *ngIf vs @if()

--- src/app/structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component.ts
+++ src/app/structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-directive-ngif-vs-if',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './structural-directive-ngif-vs-if.component.html',
   styleUrl: './structural-directive-ngif-vs-if.component.scss',
 })
 export class StructuralDirectiveNgifVsIfComponent {
   isChecked: boolean = false;
   isInputBox: boolean = true;
+  input1: string = '';
+  input2: string = '';
 
   onClick() {
     // this.isChecked = this.isChecked == false ? true : false; --> bisa begini
